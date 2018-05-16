bc0d525468b3eb7ea10914e9fb5999f7d1239fd1
brunoportela4 <brunoportelafv@gmail.com>
2018-05-15 22:23:05 -0300

    Calculadora funcionando

---
+++ calculadora/calculadora/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace calculadora
+{
+    partial class tela
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnMais = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnVezes = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.txbVisor = new System.Windows.Forms.TextBox();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.txbHistorico = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(20, 75);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(36, 36);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(62, 75);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(36, 36);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(104, 75);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(36, 36);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(20, 117);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(36, 36);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(62, 117);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(36, 36);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(104, 117);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(36, 36);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(20, 159);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(36, 36);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(62, 159);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(36, 36);
+            this.btn8.TabIndex = 7;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(104, 159);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(36, 36);
+            this.btn9.TabIndex = 8;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(62, 201);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(36, 36);
+            this.btn0.TabIndex = 9;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnMais
+            // 
+            this.btnMais.Location = new System.Drawing.Point(146, 75);
+            this.btnMais.Name = "btnMais";
+            this.btnMais.Size = new System.Drawing.Size(36, 36);
+            this.btnMais.TabIndex = 10;
+            this.btnMais.Text = "+";
+            this.btnMais.UseVisualStyleBackColor = true;
+            this.btnMais.Click += new System.EventHandler(this.btnMais_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Location = new System.Drawing.Point(146, 117);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(36, 36);
+            this.btnMenos.TabIndex = 11;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
+            // 
+            // btnVezes
+            // 
+            this.btnVezes.Location = new System.Drawing.Point(145, 201);
+            this.btnVezes.Name = "btnVezes";
+            this.btnVezes.Size = new System.Drawing.Size(36, 36);
+            this.btnVezes.TabIndex = 12;
+            this.btnVezes.Text = "*";
+            this.btnVezes.UseVisualStyleBackColor = true;
+            this.btnVezes.Click += new System.EventHandler(this.btnVezes_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(146, 159);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(36, 36);
+            this.btnDividir.TabIndex = 13;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // txbVisor
+            // 
+            this.txbVisor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbVisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbVisor.Location = new System.Drawing.Point(20, 29);
+            this.txbVisor.Name = "txbVisor";
+            this.txbVisor.Size = new System.Drawing.Size(162, 19);
+            this.txbVisor.TabIndex = 14;
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(104, 201);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(36, 36);
+            this.btnIgual.TabIndex = 15;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(20, 201);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(36, 36);
+            this.btnC.TabIndex = 16;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // txbHistorico
+            // 
+            this.txbHistorico.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbHistorico.Location = new System.Drawing.Point(20, 16);
+            this.txbHistorico.Name = "txbHistorico";
+            this.txbHistorico.Size = new System.Drawing.Size(162, 13);
+            this.txbHistorico.TabIndex = 17;
+            // 
+            // tela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(197, 255);
+            this.Controls.Add(this.txbHistorico);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.txbVisor);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnVezes);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnMais);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "tela";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnMais;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnVezes;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.TextBox txbVisor;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.TextBox txbHistorico;
+    }
+}
+
---
+++ calculadora/calculadora/Form1.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculadora
+{
+    public partial class tela : Form
+    {
+        public tela()
+        {
+            InitializeComponent();
+        }
+
+        double valor,num1,num2;
+        int operacao, v=0;
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 1;
+            txbHistorico.Text = txbHistorico.Text + 1;
+            if (v == 0)
+            {
+                if (num1 != 0) {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 1;
+            }
+            else {
+                 if (num2 != 0) {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 1;
+            }
+               
+            }
+        
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 2;
+            txbHistorico.Text = txbHistorico.Text + 2;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 2;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 2;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 3;
+            txbHistorico.Text = txbHistorico.Text + 3;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 3;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 3;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 4;
+            txbHistorico.Text = txbHistorico.Text + 4;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 4;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 4;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 5;
+            txbHistorico.Text = txbHistorico.Text + 5;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 5;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 5;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 6;
+            txbHistorico.Text = txbHistorico.Text + 6;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 6;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 6;
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 7;
+            txbHistorico.Text = txbHistorico.Text + 7;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 7;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 7;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 8;
+            txbHistorico.Text = txbHistorico.Text + 8;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 8;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 8;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 9;
+            txbHistorico.Text = txbHistorico.Text + 9;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 9;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 9;
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            txbVisor.Text = txbVisor.Text + 0;
+            txbHistorico.Text = txbHistorico.Text + 0;
+            if (v == 0)
+            {
+                if (num1 != 0)
+                {
+                    num1 = num1 * 10;
+                }
+                num1 = num1 + 0;
+            }
+            else
+            {
+                if (num2 != 0)
+                {
+                    num2 = num2 * 10;
+                }
+                num2 = num2 + 0;
+            }
+        }
+
+        private void btnMais_Click(object sender, EventArgs e)
+        {
+            if (v >= 1)
+            {
+                switch (operacao)
+                {
+                    case 1:
+                        {
+                            valor = num1 + num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 2:
+                        {
+                            valor = num1 - num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 3:
+                        {
+                            valor = num1 / num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 4:
+                        {
+                            valor = num1 * num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    default: break;
+                }
+                num1 = valor;
+                num2 = 0;
+            }
+
+            valor = float.Parse(txbVisor.Text);
+            txbVisor.Clear();
+            operacao = 1;
+            txbHistorico.Text = txbHistorico.Text + "+";
+            v++;
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            if (v >= 1)
+            {
+                switch (operacao)
+                {
+                    case 1:
+                        {
+                            valor = num1 + num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 2:
+                        {
+                            valor = num1 - num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 3:
+                        {
+                            valor = num1 / num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 4:
+                        {
+                            valor = num1 * num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    default: break;
+                }
+                num1 = valor;
+                num2 = 0;
+            }
+
+            valor = float.Parse(txbVisor.Text);
+            txbVisor.Clear();
+            operacao = 2;
+            txbHistorico.Text = txbHistorico.Text + "-";
+            v++;
+
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            if (v >= 1)
+            {
+                switch (operacao)
+                {
+                    case 1:
+                        {
+                            valor = num1 + num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 2:
+                        {
+                            valor = num1 - num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 3:
+                        {
+                            valor = num1 / num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 4:
+                        {
+                            valor = num1 * num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    default: break;
+                }
+                num1 = valor;
+                num2 = 0;
+            }
+
+            valor = float.Parse(txbVisor.Text);
+            txbVisor.Clear();
+            operacao = 3;
+            txbHistorico.Text = txbHistorico.Text + "/";
+            v++;
+        }
+
+        private void btnVezes_Click(object sender, EventArgs e)
+        {
+            if (v >= 1)
+            {
+                switch (operacao)
+                {
+                    case 1:
+                        {
+                            valor = num1 + num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 2:
+                        {
+                            valor = num1 - num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 3:
+                        {
+                            valor = num1 / num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    case 4:
+                        {
+                            valor = num1 * num2;
+                            txbVisor.Text = valor.ToString();
+                            break;
+                        }
+                    default: break;
+                }
+                num1 = valor;
+                num2 = 0;
+            }
+
+            valor = float.Parse(txbVisor.Text);
+            txbVisor.Clear();
+            operacao = 4;
+            txbHistorico.Text = txbHistorico.Text + "*";
+            v++;
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            switch (operacao) {
+                case 1: {
+                    valor = num1+num2;
+                    txbVisor.Text = valor.ToString();
+                    break;
+                }
+                case 2:
+                    {
+                        valor = num1-num2;
+                        txbVisor.Text = valor.ToString();
+                        break;
+                    }
+                case 3:
+                    {
+                        valor = num1/num2;
+                        txbVisor.Text = valor.ToString();
+                        break;
+                    }
+                case 4:
+                    {
+                        valor = num1*num2;
+                        txbVisor.Text = valor.ToString();
+                        break;
+                    }
+                default : break;
+            }
+            num1 = valor;
+            num2 = 0;
+        }
+
+        private void btnC_Click(object sender, EventArgs e)
+        {
+            txbVisor.Clear();
+            txbHistorico.Clear();
+        }
+    }
+}
