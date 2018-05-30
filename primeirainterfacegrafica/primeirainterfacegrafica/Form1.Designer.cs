bd549decacc6000a637c2f995436d6bc5a5ef318
Bruno Portela <31595407+brunoportela4@users.noreply.github.com>
2018-05-29 21:58:07 -0300

    interface gráfica

---
+++ primeirainterfacegrafica/primeirainterfacegrafica/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace primeirainterfacegrafica
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSobrenome = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.txbSobrenome = new System.Windows.Forms.TextBox();
+            this.txbV1 = new System.Windows.Forms.TextBox();
+            this.txbV2 = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblResultCalc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(103, 166);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(41, 29);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(38, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblSobrenome
+            // 
+            this.lblSobrenome.AutoSize = true;
+            this.lblSobrenome.Location = new System.Drawing.Point(13, 90);
+            this.lblSobrenome.Name = "lblSobrenome";
+            this.lblSobrenome.Size = new System.Drawing.Size(64, 13);
+            this.lblSobrenome.TabIndex = 2;
+            this.lblSobrenome.Text = "Sobrenome:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(41, 212);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 3;
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(83, 26);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(100, 20);
+            this.txbNome.TabIndex = 4;
+            // 
+            // txbSobrenome
+            // 
+            this.txbSobrenome.Location = new System.Drawing.Point(83, 87);
+            this.txbSobrenome.Name = "txbSobrenome";
+            this.txbSobrenome.Size = new System.Drawing.Size(100, 20);
+            this.txbSobrenome.TabIndex = 5;
+            // 
+            // txbV1
+            // 
+            this.txbV1.Location = new System.Drawing.Point(443, 29);
+            this.txbV1.Name = "txbV1";
+            this.txbV1.Size = new System.Drawing.Size(100, 20);
+            this.txbV1.TabIndex = 6;
+            // 
+            // txbV2
+            // 
+            this.txbV2.Location = new System.Drawing.Point(442, 90);
+            this.txbV2.Name = "txbV2";
+            this.txbV2.Size = new System.Drawing.Size(100, 20);
+            this.txbV2.TabIndex = 7;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(466, 165);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calcular";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblResultCalc
+            // 
+            this.lblResultCalc.AutoSize = true;
+            this.lblResultCalc.Location = new System.Drawing.Point(505, 212);
+            this.lblResultCalc.Name = "lblResultCalc";
+            this.lblResultCalc.Size = new System.Drawing.Size(0, 13);
+            this.lblResultCalc.TabIndex = 9;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 262);
+            this.Controls.Add(this.lblResultCalc);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txbV2);
+            this.Controls.Add(this.txbV1);
+            this.Controls.Add(this.txbSobrenome);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblSobrenome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSobrenome;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.TextBox txbSobrenome;
+        private System.Windows.Forms.TextBox txbV1;
+        private System.Windows.Forms.TextBox txbV2;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblResultCalc;
+    }
+}
+
---
+++ primeirainterfacegrafica/primeirainterfacegrafica/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace primeirainterfacegrafica
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Seu nome completo é: "+ txbNome.Text + " " + txbSobrenome.Text;
+            MessageBox.Show("Seu nome completo é: "+ txbNome.Text + " " + txbSobrenome.Text);
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int v1 = System.Convert.ToInt32(txbV1.Text);
+            int v2 = System.Convert.ToInt32(txbV2.Text);
+            int result;
+
+            result = v1 + v2;
+
+            lblResultCalc.Text = "O resultado da soma é: " + result;
+
+        }
+
+
+    }
+}
